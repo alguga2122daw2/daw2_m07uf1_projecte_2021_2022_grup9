7976dec0bf100fc723f5df2cc920fb206a40c85c
albert <allade2122daw2@protonmail.com>
2021-12-08 18:04:54 +0100

    Añadida estructura de botones y dos archivos

---
+++ pagina1.php
@@ -0,0 +1,11 @@
+<?php include("templates/top.php");?>
+
+    <div id="container">
+        <a><input type="submit" value="LLIBRES"/></a>
+        <a><input type="submit" value="USUARIS"/></a>
+        <a><input type="submit" value="BIBLIOTECARIS"/></a>
+        <a><input type="submit" value="DADES PERSONALS"/></a>
+    </div>
+
+
+<?php include("templates/bottom.php")?>
